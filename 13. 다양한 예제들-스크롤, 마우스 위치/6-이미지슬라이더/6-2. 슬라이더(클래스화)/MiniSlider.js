ea2bc3e2058a8f0154f81fd99792226cc854e222
chaehyeon <kch971109@naver.com>
2025-08-27 00:45:38 +0900

    :memo: JavaScript 비동기 학습 & README 업데이트

--- 13. 다양한 예제들-스크롤, 마우스 위치/6-이미지슬라이더/6-2. 슬라이더(클래스화)/MiniSlider.js
+++ 13. 다양한 예제들-스크롤, 마우스 위치/6-이미지슬라이더/6-2. 슬라이더(클래스화)/MiniSlider.js
@@ -1,8 +1,29 @@
-export class MiniSlider {
+export default class MiniSlider {
   constructor({ containerSelector, data, interval = 2000 }) {
     this.container = document.querySelector(containerSelector);
     if (!this.container) {
       throw new Error("컨테이너가 없습니다.");
     }
+
+    this.data = data;
+    this.interval = interval;
+    this.currentIndex = 0;
+    this.paginationItemArr = [];
+
+    this.slideContainer = document.createElement("div");
+    this.slide = document.createElement("div");
+    this.slideWrapper = document.createElement("div");
+    this.slidePagination = document.createElement("div");
+
+    this.slideContainer.classList.add("slide-container");
+    this.slide.classList.add("slide");
+    this.slideWrapper.classList.add("slide-wrapper");
+    this.slidePagination.classList.add("slide-pagination");
+
+    this.slide.append(slideWrapper);
+    this.slideContainer.append(slide);
+    this.slideContainer.append(slidePagination);
+
+    this.container.append(slideContainer);
   }
 }
